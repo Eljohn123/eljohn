53dab52403adbb3b9b81e320c869d637441eea79
eljohn <eljohn.torres@gsfe.tupcavite.edu.ph>
2021-04-27 00:01:52 +0800

    commit today

--- FunctionalTest.py
+++ FunctionalTest.py
@@ -8,9 +8,10 @@ class PageTest(unittest.TestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
+		self.browser.implicitly_wait(3)
 
-	#def tearDown(self):
-	#	self.browser.quit()
+	def tearDown(self):
+		self.browser.quit()
 
 	#def test_browser_title(self):
 		#self.browser.get('http://localhost:8000')
